fe461dc9a476a7bc53e79051fbabb069ef495179
Tripathi <Varish.Tripathi@bain.com>
2025-08-30 13:43:50 +0530

    Streamlit_added

--- streamlit_front_end.py
+++ streamlit_front_end.py
@@ -1,7 +1,38 @@
 import streamlit as st
 
-with st.chat_message('user'):
-    st.text('Hi')
+# with st.chat_message('user'):
+#     st.text('Hi')
 
-with st.chat_message('assistant'):
-    st.text('How can i help you')
+# with st.chat_message('assistant'):
+#     st.text('How can i help you')
+
+
+# user_input = st.chat_input('type here')
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+
+# loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+
+user_input = st.chat_input('type here')
+
+if user_input:
+    # first add the message to message_history
+    st.session_state['message_history'].append(
+        {'role': 'user', 'content': user_input}
+    )
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    # first add the message to message_history
+    st.session_state['message_history'].append(
+        {'role': 'assistant', 'content': user_input}
+    )
+    with st.chat_message('assistant'):
+        st.text(user_input)
+
